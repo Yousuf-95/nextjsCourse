7b55a14ba6b6ff425e981498ab5b5e7a3ebfc5aa
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-09-20 13:43:43 +0530

    create routes in pages directory

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+function CatchAllRoutes() {
+    return(
+        <>
+            <div>
+                <h1>Page to catch all routes</h1>
+            </div>
+        </>
+    );
+}
+
+export default CatchAllRoutes;
---
+++ pages/events/[eventId].js
@@ -0,0 +1,11 @@
+function SpecificEventPage() {
+    return(
+        <>
+            <div>
+                <h1>Specific Event Page</h1>
+            </div>
+        </>
+    );
+}
+
+export default SpecificEventPage;
