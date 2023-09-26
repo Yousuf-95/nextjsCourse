5c95187e9067adb8b174ecdd3eba49db8fe55a77
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-09-26 21:49:10 +0530

    static generation example

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
+import fs from "fs/promises";
+import path from "path";
+
 function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+// The code in this function won't be visible in the frontend.
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummyBackend.json");
+  console.log(filePath);
+  const result = await fs.readFile(filePath);
+  const data = JSON.parse(result);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
 
 export default HomePage;
