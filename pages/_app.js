fd29c91080b81c0681a614334b69cce4ec23b86d
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-10-07 22:55:01 +0530

    initialize repo for section-8

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
-import Layout from "@/components/layout/layout";
-import Head from "next/head";
-import "@/styles/globals.css";
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name="description" content="Nextjs events" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" key="description" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,9 @@
-import EventList from "@/components/events/eventList";
-import connectDb from "../lib/mongodbConnect.js";
-import EventsModel from "../models/eventsModel.js";
-import Head from "next/head.js";
-
-function HomePage(props) {
-  const { featuredEvents } = props;
-
+function HomePage() {
   return (
-    <>
-      <div>
-        <Head>
-          <title>Events Home Page</title>
-          <meta
-            name="description"
-            content="View open events"
-            key="description"
-          />
-        </Head>
-        <EventList items={featuredEvents} />
-      </div>
-    </>
+    <div>
+      <h1>The Home Page</h1>
+    </div>
   );
 }
 
 export default HomePage;
-
-export async function getStaticProps() {
-  try {
-    await connectDb();
-
-    const result = await EventsModel.find(
-      { isFeatured: true }
-      // { _id: 0 }
-    ).lean();
-
-    // Either Convert _id to string or remove '_id' field from query.
-    // Nextjs will throw an error otherwise.
-    const events = result.map((event) => {
-      return JSON.parse(JSON.stringify(event));
-    });
-
-    return {
-      props: {
-        featuredEvents: events,
-      },
-      revalidate: 10800,
-    };
-  } catch (error) {
-    console.log(error);
-  }
-}
