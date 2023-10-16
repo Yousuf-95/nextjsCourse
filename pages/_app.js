060478c8f87e7bd18caa96c0c437e68f90a07ba3
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-10-16 21:29:34 +0530

    setup notification context

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,23 @@
 import Layout from "@/components/layout/layout";
 import Head from "next/head";
 import "@/styles/globals.css";
+import { NotificationContextProvider } from "@/store/notification-context";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name="description" content="Nextjs events" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" key="description" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="Nextjs events" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+            key="description"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
