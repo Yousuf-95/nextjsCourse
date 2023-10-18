151288f2cb8c5cd697a59e3969d37c7412d78ca9
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-10-18 19:18:51 +0530

    initialize project for section-11

--- models/userModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-  email: String,
-  signupDate: { type: Date, default: Date.now },
-});
-
-export default mongoose.models.users || mongoose.model("users", UserSchema);
