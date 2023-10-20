bcb43b364ca4fdea129bf9e7367c25492fe00993
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-10-20 21:14:35 +0530

    feat: add featured posts to home page

---
+++ components/posts/postsGrid.js
@@ -0,0 +1,16 @@
+import PostItem from "./postItem";
+import styles from "./postsGrid.module.css";
+
+function PostsGrid(props) {
+  const { posts } = props;
+
+  return (
+    <ul className={styles.grid}>
+      {posts.map((post) => (
+        <PostItem key={post.slug} post={post} />
+      ))}
+    </ul>
+  );
+}
+
+export default PostsGrid;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import Hero from "@/components/homePage/hero";
+import FeaturedPosts from "@/components/homePage/featuredPosts";
+
+const DUMMY_POSTS = [
+  {
+    title: "Getting Started with NextJS",
+    slug: "getting-started-nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta autem facere error magni, harum iure.",
+    date: "2023-10-04",
+  },
+  {
+    title: "Getting Started with NextJS",
+    slug: "getting-started-nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, minima labore quod natus possimus sequi.",
+    date: "2023-10-08",
+  },
+];
 
 function HomePage() {
   return (
     <>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </>
   );
 }
