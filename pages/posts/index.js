1bef9cb6eb56ba3c1041a765a0acd015565d9d2f
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-10-21 21:38:08 +0530

    feat: all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,26 @@
-function AllPostsPage() {}
+import AllPosts from "@/components/posts/allPosts";
+
+const DUMMY_POSTS = [
+  {
+    title: "Getting Started with NextJS",
+    slug: "getting-started-nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta autem facere error magni, harum iure.",
+    date: "2023-10-04",
+  },
+  {
+    title: "Getting Started with NextJS",
+    slug: "getting-started-nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, minima labore quod natus possimus sequi.",
+    date: "2023-10-08",
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
