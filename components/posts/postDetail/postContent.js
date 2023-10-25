1dd1505b1c2f0241d5f3f3fa87432b66fa3ad9de
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-10-25 21:12:18 +0530

    feat: render image in .md with nextjs image component

--- components/posts/postDetail/postContent.js
+++ components/posts/postDetail/postContent.js
@@ -1,15 +1,49 @@
 import ReactMarkdown from "react-markdown";
 import PostHeader from "./postHeader";
 import styles from "./postContent.module.css";
+import Image from "next/image";
 
 function PostContent(props) {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    // img: function (image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p: function (paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={styles.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={styles.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
