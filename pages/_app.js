d423c09c1480b559f64aca193415dd9cf1556fda
Yousuf Ahmad <yf.yousuf95@gmail.com>
2023-11-16 21:55:57 +0530

    feat: add next-auth session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
+import { Provider } from 'next-auth/react';
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+
+    </Provider>
   );
 }
 
